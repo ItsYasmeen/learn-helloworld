7306a8e451b77f13bf6c4e628c027163513de8fa
ItsYasmeen <97419996+ItsYasmeen@users.noreply.github.com>
2022-03-18 15:17:16 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+t = 0
+def helloworld():
+    for a in range(1,101):
+        if a%2 ==0:
+            print(str(a) +"--even")
+        else:
+            print(str(a) +"--odd")
+    print("Hello World!")
+
+helloworld()
